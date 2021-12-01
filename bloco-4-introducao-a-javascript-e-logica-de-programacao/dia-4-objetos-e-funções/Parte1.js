3683f6fe546c81c1b7ec974bd3b70a0919a41d3d
Henrique <henrique.campana22@gmail.com>
2021-12-01 18:10:57 -0300

    Faz exercicio 3 - parte 1

--- bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-objetos-e-funções/Parte1.js
+++ bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-objetos-e-funções/Parte1.js
@@ -1,12 +1,19 @@
 let info = {
-    personagem: 'Margarida',
-    origem: 'Pato Donald',
-    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+	personagem: 'Margarida',
+	origem: 'Pato Donald',
+	nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald'
 };
 
 //exercicio 1
-    // console.log('Bem-vinda, ' + info.personagem);
+
+//console.log('Bem-vinda, ' + info.personagem);
 
 //exercicio 2
-    info.recorrente = 'Sim';
-    console.log(info);
+
+info.recorrente = 'Sim';
+//console.log(info);
+
+//exercicio 3
+for (keys in info){
+    console.log(keys);
+}
