c69529e602232a7774451da3e18283e336f36187
Henrique <henrique.campana22@gmail.com>
2021-12-01 18:24:55 -0300

    Faz exercicio 5 - Parte 1

--- bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-objetos-e-funções/Parte1.js
+++ bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-objetos-e-funções/Parte1.js
@@ -21,6 +21,29 @@ info.recorrente = 'Sim';
 
 //exercicio 4
 
+// for (keys in info){
+//     console.log(info[keys]);
+// }
+
+//exercicio 5
+
+let infoPatinhas = {
+	personagem: 'Tio Patinhas',
+	origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+	nota: 'O último MacPatinhas',
+	recorrente: 'Sim'
+};
+
+let recorrenteMargarida = info.recorrente;
+let recorrenteTioPatinhas = infoPatinhas.recorrente;
+
+delete info.recorrente;
+delete infoPatinhas.recorrente;
+
 for (keys in info){
-    console.log(info[keys]);
+    console.log(info[keys] + ' e ' + infoPatinhas[keys]);
+}
+
+if (recorrenteMargarida === 'Sim' && recorrenteTioPatinhas === 'Sim') {
+    console.log('Ambos recorrentes');
 }
